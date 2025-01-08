bcfbbffe494fb952f6c44bfe6d19e418e126d912
Apanovich86 <apanovich.anna.itec@gmail.com>
2025-01-08 22:55:43 +0100

    react-useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import './App.css'
-import MyComponent from "./components/MyComponent.tsx";
+import {useState} from "react";
 
 function App() {
 
-  return (
-    <>
-     <MyComponent text={'test'}/>
-    </>
-  )
+    let [counter, setCounter] = useState(0);
+    return (
+        <>
+            <h2>{counter}</h2>
+            <button onClick={() => {
+              setCounter(++counter);
+            }}>increment
+            </button>
+            <button onClick={() => {
+               setCounter(--counter);
+            }}>decrement
+            </button>
+        </>
+    )
 }
 
 export default App
