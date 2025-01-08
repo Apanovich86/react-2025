b4630df68cf251f9dfe7f195e67790014b2b2b4a
Apanovich86 <apanovich.anna.itec@gmail.com>
2025-01-08 21:37:08 +0100

    react-components-tailwind-map

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
+
+function App() {
+
+  return (
+    <>
+     <MyComponent text={'test'}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,5 @@
+ function MyComponent ({text}: {text: string}) {
+    return <div className='text-3xl font-bold underline'>{text}</div>
+}
+
+export default MyComponent;
